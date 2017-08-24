d4f3aa277dd84a675dbfac6a555d56ef7a74c493
Rakan Dirbas <rakan.dirbas@gmail.com>
2017-08-24 21:12:59 +0200

    improved project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='linkpredpy',
+   version='0.1',
+   description='A link prediction library',
+   author='Rakan Dirbas',
+   packages=['linkpredpy'],
+   install_requires=['numpy', 'scipy', 'networkx']
+)
